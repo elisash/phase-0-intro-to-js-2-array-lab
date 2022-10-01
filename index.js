4fdedef03cc01a42aa51ee60b8fd1d4c1934b2b1
Elisa Situ Huang <elisasituhuang@gmail.com>
2022-09-30 23:34:22 -0400

    commit tree 20ecfad44bd005a1eace97bcaa5cc88bf65f03bf

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+   console.log (cats.push(name));
+}
+function destructivelyPrependCat(name) {
+    console.log (cats.unshift(name));
+}
+function  destructivelyRemoveLastCat() {
+    console.log (cats.pop());
+}
+function  destructivelyRemoveFirstCat() {
+    console.log (cats.shift());
+}
+function appendCat(name) {
+var newArray = cats.slice();
+newArray.push(name)
+return newArray
+}
+function prependCat(name) {
+    var newRrray = cats.slice();
+    newRrray.unshift(name)
+    return newRrray
+    }
+    function removeLastCat() {
+        var rLastCat = cats.slice();
+        rLastCat.pop()
+        return rLastCat
+
+    }
+    function removeFirstCat() {
+        var rFirstCat = cats.slice();
+        rFirstCat.shift()
+        return rFirstCat
+    }
